149e20938d196cfc529caa7690f58f739e14ea8e
Yuulis <yuulis2005@gmail.com>
2022-02-12 17:19:22 +0900

    Cardクラスのコンストラクタの作成

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,6 +7,12 @@ public class Card : MonoBehaviour
     string m_color;
     string m_value;
 
+    public Card(string color, string value)
+    {
+        this.m_color = color;
+        this.m_value = value;
+    }
+
     bool EvaluateCard(string open_color, string open_value)
     {
         if (this.m_color == open_color || this.m_value == open_value || this.m_value == "ch" || this.m_value == "wdr4") return true;
